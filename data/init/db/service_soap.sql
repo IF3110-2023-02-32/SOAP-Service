7b910aa1f4556ffedfa3449b5dc16c6cbefd8255
fazelginanda <fzginanda@gmail.com>
2023-11-17 01:19:29 +0700

    fix: change database init

--- data/init/db/service_soap.sql
+++ data/init/db/service_soap.sql
@@ -5,18 +5,21 @@ CREATE TABLE IF NOT EXISTS `logging` (
   `endpoint` varchar(255) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 CREATE TABLE IF NOT EXISTS `unlocking` (
   `socmed_id` int NOT NULL,
   `dashboard_id` int DEFAULT NULL,
   `link_code` varchar(8) NOT NULL UNIQUE,
-  PRIMARY KEY (`creator_id`, `subscriber_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`socmed_id`)
+);
 
 CREATE TABLE IF NOT EXISTS `apikey` (
   `id` int NOT NULL AUTO_INCREMENT,
   `key` varchar(255) NOT NULL UNIQUE,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
+
+INSERT INTO apikey(`key`, `timestamp`) VALUES ('abcdefgh12345678', CURRENT_TIMESTAMP),
+('ijklmnop12345678', CURRENT_TIMESTAMP);
